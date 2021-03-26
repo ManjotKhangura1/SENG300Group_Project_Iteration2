6b35c51219ac80b352f9df26ffc4c118be8abbb9
ljmay <ljmay@10.0.0.65>
2021-03-26 10:52:54 -0600

    Set up finishing adding items

--- SelfCheckoutSystem - Software/src/FinishesAddingItems.java
+++ SelfCheckoutSystem - Software/src/FinishesAddingItems.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+
+import org.lsmr.selfcheckout.devices.*;
+import org.lsmr.selfcheckout.devices.listeners.*;
+import org.lsmr.selfcheckout.*;
+
 
 public class FinishesAddingItems {
 
+	public FinishesAddingItems(SelfCheckoutStation station) {
+		
+	}
 }

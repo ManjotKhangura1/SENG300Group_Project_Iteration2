92c6b179cea0aa3a5fe42828d992e857dd005a79
ljmay <ljmay@10.0.0.65>
2021-03-27 16:21:40 -0600

    added print receipt functionality

--- SelfCheckoutSystem - Software/src/FinishesAddingItems.java
+++ SelfCheckoutSystem - Software/src/FinishesAddingItems.java
@@ -6,9 +6,12 @@ import org.lsmr.selfcheckout.*;
 
 
 public class FinishesAddingItems {
+	ReceiptPrinter printer;
+	ScanItem scan;
+	boolean isEnabled;
 
 	/**
-	 * Constructor to finish adding items
+	 * Constructor to finish transaction
 	 * @param SelfCheckoutStation 
 	 * @param ScanItem
 	 */
@@ -16,6 +19,8 @@ public class FinishesAddingItems {
 		scan.handheld = null;
 		scan.main = null;
 		
+		printer = station.printer;
+		this.scan = scan;
 	}
 	  
 	/**
@@ -23,5 +28,47 @@ public class FinishesAddingItems {
 	 */
 	public void checkForMembership() {
 		
+	}
+	
+	/**
+	 * prints the user receipt
+	 */
+	public void printRecipt() {
+		
+		String str = scan.getTotalList().toString();
+		for(int i = 0; i < str.length(); i++) {
+				printer.print(str.charAt(i));
+		}
+		
+		printer.cutPaper();
+		printer.removeReceipt();
+	}
+	
+	private void printerListener() {
+		printer.register(new ReceiptPrinterListener() {
+
+			@Override
+			public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+				isEnabled = true;
+			}
+
+			@Override
+			public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+				isEnabled = false;
+			}
+
+			@Override
+			public void outOfPaper(ReceiptPrinter printer) {}
+
+			@Override
+			public void outOfInk(ReceiptPrinter printer) {}
+
+			@Override
+			public void paperAdded(ReceiptPrinter printer) {}
+
+			@Override
+			public void inkAdded(ReceiptPrinter printer) {}
+			
+		});
 	}
 }

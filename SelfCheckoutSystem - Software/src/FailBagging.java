d2cc911b3f86610edbcd98430c4d4b417ea30678
ManjotKhangura1 <59262828+ManjotKhangura1@users.noreply.github.com>
2021-03-30 20:41:15 -0600

    Commented FailBagging

--- SelfCheckoutSystem - Software/src/FailBagging.java
+++ SelfCheckoutSystem - Software/src/FailBagging.java
@@ -7,20 +7,32 @@ import org.lsmr.selfcheckout.devices.listeners.ElectronicScaleListener;
 
 public class FailBagging {
 
-	private ElectronicScale baggingArea;
-	private SelfCheckoutStation selfCheckout;
+	private ElectronicScale baggingArea;	
+	private SelfCheckoutStation selfCheckout;	
 	private Item item;
-	private double itemWeight = 0;
-	private boolean lock;
-	private double weightBeforeChange = 0;
+	private double itemWeight = 0;				//Weight of item being put in bagging area
+	private boolean lock;						//Checks if scanner is locked
+	private double weightBeforeChange = 0;		//Weight of bagging area before item is added
 	
+	
+	/**
+	 * Constructor for FailBagging
+	 * 
+	 * @param selfCheckout - self checkout station object
+	 */
 	public FailBagging(SelfCheckoutStation selfCheckout)
 	{
+		//Gets bagging area from self checkout station
 		this.baggingArea = selfCheckout.baggingArea;
+		
 		this.selfCheckout = selfCheckout;
+		
+		//Register the listener
 		this.baggingArea.register(createBaggingListener());
+		
 		try
 		{
+			//Gets current weight of bagging area
 			weightBeforeChange = baggingArea.getCurrentWeight();
 		}
 		catch (Exception e)
@@ -30,10 +42,18 @@ public class FailBagging {
 		}
 	}
 
+	/**
+	 * Getting for checking if scanner is locked
+	 * @return lock - boolean for checking if scanner is locked
+	 */
 	public boolean isLock() {
 		return lock;
 	}
 	
+	/**
+	 * Creates a listener by returning the ElectronicScaleListener
+	 * @return ElectonicScaleListener - Listener for the ElectronicScale class
+	 */
 	private ElectronicScaleListener createBaggingListener()
 	{
 		return new ElectronicScaleListener() {
@@ -52,6 +72,7 @@ public class FailBagging {
 
 			@Override
 			public void weightChanged(ElectronicScale scale, double weightInGrams) {
+				//Checks if the item was properly added to bagging area to unlock the scanners
 				if ((itemWeight + weightBeforeChange) == weightInGrams)
 				{
 					if (isLock())
@@ -78,6 +99,10 @@ public class FailBagging {
 	}
 	
 	
+	/**
+	 * Locks the scanners
+	 * @param item - item we are looking for to put in bagging area
+	 */
 	public void lockScan(Item item)
 	{
 		lock = true;
@@ -86,6 +111,9 @@ public class FailBagging {
 		selfCheckout.handheldScanner.disable();
 	}
 	
+	/**
+	 * Unlocks the scanner and resets item weight to 0 for next item to be added
+	 */
 	private void unlockScan()
 	{
 		lock = false;
@@ -94,6 +122,9 @@ public class FailBagging {
 		selfCheckout.handheldScanner.enable();
 	}
 	
+	/**
+	 * Used to override the scanners and unlock by admin for other potential reasons
+	 */
 	public void adminOverride()
 	{
 		if (isLock())

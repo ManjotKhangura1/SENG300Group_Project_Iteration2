f8a32a6384cdc13019a1c5623f57b7d12ec5e127
Daniel <danielchau88@gmail.com>
2021-03-31 00:02:04 -0600

    added test

--- SelfCheckoutSystem - Tests/src/PayWithDebitTest.java
+++ SelfCheckoutSystem - Tests/src/PayWithDebitTest.java
@@ -1,22 +1,228 @@
 import static org.junit.Assert.*;
 
+import java.awt.image.BufferedImage;
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Currency;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.lsmr.selfcheckout.Card;
+import org.lsmr.selfcheckout.ChipFailureException;
+import org.lsmr.selfcheckout.InvalidPINException;
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+import org.lsmr.selfcheckout.external.CardIssuer;
+import org.lsmr.selfcheckout.devices.SimulationException;
+
 
 public class PayWithDebitTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	public void testPayWithSwipe() {
-		fail("Not yet implemented");
+		//SETUP.......................................................................................
+				SelfCheckoutStation testStation;
+				CardIssuer testIssuer;
+
+				//create a self checkout station and the components necessary to create it
+				Currency currency = Currency.getInstance("CAD");
+				int[] noteDenominations = {5,10,20,50,100};
+				BigDecimal[] coinDenomonations = {BigDecimal.valueOf(0.05) , BigDecimal.valueOf(0.10), BigDecimal.valueOf(0.25), 
+						BigDecimal.valueOf(1.0), BigDecimal.valueOf(2.0)};
+				int maxWeight = 23000;
+				int scaleSensitivity = 10;
+				testStation = new SelfCheckoutStation(currency, noteDenominations, coinDenomonations, maxWeight,scaleSensitivity);
+				
+				//create an issuer to manage payments
+				testIssuer = new CardIssuer("testIssuer");
+				Calendar testCalendar =  Calendar.getInstance();
+				testCalendar.set(Calendar.YEAR, 2030);
+				testIssuer.addCardData("111111", "Person One", testCalendar, "111", BigDecimal.valueOf(1000.0));
+				//.............................................................................................
+				
+				BufferedImage testSignature = new BufferedImage(10, 10, 1);
+				PayWithDebit debit = new PayWithDebit(testStation);
+				Card testCard = new Card("debit", "111111", "Person One", "111", "1234", true, true);
+			
+				assertTrue(debit.PayWithSwipe(testCard, testIssuer, testSignature, BigDecimal.valueOf(100.0)));	
+	}
+	
+
+	@Test
+	public void testPayWithTap() {
+		//SETUP.......................................................................................
+				SelfCheckoutStation testStation;
+				CardIssuer testIssuer;
+
+				//create a self checkout station and the components necessary to create it
+				Currency currency = Currency.getInstance("CAD");
+				int[] noteDenominations = {5,10,20,50,100};
+				BigDecimal[] coinDenomonations = {BigDecimal.valueOf(0.05) , BigDecimal.valueOf(0.10), BigDecimal.valueOf(0.25), 
+						BigDecimal.valueOf(1.0), BigDecimal.valueOf(2.0)};
+				int maxWeight = 23000;
+				int scaleSensitivity = 10;
+				testStation = new SelfCheckoutStation(currency, noteDenominations, coinDenomonations, maxWeight,scaleSensitivity);
+				
+				//create an issuer to manage payments
+				testIssuer = new CardIssuer("testIssuer");
+				Calendar testCalendar =  Calendar.getInstance();
+				testCalendar.set(Calendar.YEAR, 2030);
+				testIssuer.addCardData("111111", "Person One", testCalendar, "111", BigDecimal.valueOf(1000.0));
+				//.............................................................................................
+				
+				BufferedImage testSignature = new BufferedImage(10, 10, 1);
+				PayWithDebit debit = new PayWithDebit(testStation);
+				Card testCard = new Card("debit", "111111", "Person One", "111", "1234", true, true);
+			
+				assertTrue(debit.PayWithTap(testCard, testIssuer, BigDecimal.valueOf(100.0)));	
+	}
+	
+
+	@Test
+	public void testPayWithChip() {
+		//SETUP.......................................................................................
+				SelfCheckoutStation testStation;
+				CardIssuer testIssuer;
+
+				//create a self checkout station and the components necessary to create it
+				Currency currency = Currency.getInstance("CAD");
+				int[] noteDenominations = {5,10,20,50,100};
+				BigDecimal[] coinDenomonations = {BigDecimal.valueOf(0.05) , BigDecimal.valueOf(0.10), BigDecimal.valueOf(0.25), 
+						BigDecimal.valueOf(1.0), BigDecimal.valueOf(2.0)};
+				int maxWeight = 23000;
+				int scaleSensitivity = 10;
+				testStation = new SelfCheckoutStation(currency, noteDenominations, coinDenomonations, maxWeight,scaleSensitivity);
+				
+				//create an issuer to manage payments
+				testIssuer = new CardIssuer("testIssuer");
+				Calendar testCalendar =  Calendar.getInstance();
+				testCalendar.set(Calendar.YEAR, 2030);
+				testIssuer.addCardData("111111", "Person One", testCalendar, "111", BigDecimal.valueOf(1000.0));
+				//.............................................................................................
+				
+				BufferedImage testSignature = new BufferedImage(10, 10, 1);
+				PayWithDebit debit = new PayWithDebit(testStation);
+				Card testCard = new Card("debit", "111111", "Person One", "111", "1234", true, true);
+			
+				assertTrue(debit.PayWithInsert(testCard, testIssuer, BigDecimal.valueOf(100.0), "1234"));	
+	}
+	
+
+	
+	@Test(expected = SimulationException.class)
+	public void testInvalidBalanceSwipe() {
+		//SETUP.......................................................................................
+				SelfCheckoutStation testStation;
+				CardIssuer testIssuer;
+
+				//create a self checkout station and the components necessary to create it
+				Currency currency = Currency.getInstance("CAD");
+				int[] noteDenominations = {5,10,20,50,100};
+				BigDecimal[] coinDenomonations = {BigDecimal.valueOf(0.05) , BigDecimal.valueOf(0.10), BigDecimal.valueOf(0.25), 
+						BigDecimal.valueOf(1.0), BigDecimal.valueOf(2.0)};
+				int maxWeight = 23000;
+				int scaleSensitivity = 10;
+				testStation = new SelfCheckoutStation(currency, noteDenominations, coinDenomonations, maxWeight,scaleSensitivity);
+				
+				//create an issuer to manage payments
+				testIssuer = new CardIssuer("testIssuer");
+				Calendar testCalendar =  Calendar.getInstance();
+				testCalendar.set(Calendar.YEAR, 2030);
+				testIssuer.addCardData("111111", "Person One", testCalendar, "111", BigDecimal.valueOf(5.0));
+				//.............................................................................................
+				
+				BufferedImage testSignature = new BufferedImage(10, 10, 1);
+				PayWithDebit debit = new PayWithDebit(testStation);
+				Card testCard = new Card("debit", "111111", "Person One", "111", "1234", true, true);
+			
+				debit.PayWithSwipe(testCard, testIssuer, testSignature, BigDecimal.valueOf(100.0));
+	}
+	
+	@Test(expected = SimulationException.class)
+	public void testInvalidBalanceTap() {
+		//SETUP.......................................................................................
+				SelfCheckoutStation testStation;
+				CardIssuer testIssuer;
+
+				//create a self checkout station and the components necessary to create it
+				Currency currency = Currency.getInstance("CAD");
+				int[] noteDenominations = {5,10,20,50,100};
+				BigDecimal[] coinDenomonations = {BigDecimal.valueOf(0.05) , BigDecimal.valueOf(0.10), BigDecimal.valueOf(0.25), 
+						BigDecimal.valueOf(1.0), BigDecimal.valueOf(2.0)};
+				int maxWeight = 23000;
+				int scaleSensitivity = 10;
+				testStation = new SelfCheckoutStation(currency, noteDenominations, coinDenomonations, maxWeight,scaleSensitivity);
+				
+				//create an issuer to manage payments
+				testIssuer = new CardIssuer("testIssuer");
+				Calendar testCalendar =  Calendar.getInstance();
+				testCalendar.set(Calendar.YEAR, 2030);
+				testIssuer.addCardData("111111", "Person One", testCalendar, "111", BigDecimal.valueOf(5.0));
+				//.............................................................................................
+				
+				PayWithDebit debit = new PayWithDebit(testStation);
+				Card testCard = new Card("debit", "111111", "Person One", "111", "1234", true, true);
+			
+				debit.PayWithTap(testCard, testIssuer, BigDecimal.valueOf(100.0));
+	}
+	
+	
+	@Test(expected = SimulationException.class)
+	public void testInvalidBalanceInsert() {
+		//SETUP.......................................................................................
+				SelfCheckoutStation testStation;
+				CardIssuer testIssuer;
+
+				//create a self checkout station and the components necessary to create it
+				Currency currency = Currency.getInstance("CAD");
+				int[] noteDenominations = {5,10,20,50,100};
+				BigDecimal[] coinDenomonations = {BigDecimal.valueOf(0.05) , BigDecimal.valueOf(0.10), BigDecimal.valueOf(0.25), 
+						BigDecimal.valueOf(1.0), BigDecimal.valueOf(2.0)};
+				int maxWeight = 23000;
+				int scaleSensitivity = 10;
+				testStation = new SelfCheckoutStation(currency, noteDenominations, coinDenomonations, maxWeight,scaleSensitivity);
+				
+				//create an issuer to manage payments
+				testIssuer = new CardIssuer("testIssuer");
+				Calendar testCalendar =  Calendar.getInstance();
+				testCalendar.set(Calendar.YEAR, 2030);
+				testIssuer.addCardData("111111", "Person One", testCalendar, "111", BigDecimal.valueOf(5.0));
+				//.............................................................................................
+				
+				PayWithDebit debit = new PayWithDebit(testStation);
+				Card testCard = new Card("debit", "111111", "Person One", "111", "1234", true, true);
+			
+				debit.PayWithInsert(testCard, testIssuer, BigDecimal.valueOf(100.0), "1234");
 	}
 
+	
+	@Test(expected = SimulationException.class)
+	public void testTapNotEnabled() {
+		//SETUP.......................................................................................
+				SelfCheckoutStation testStation;
+				CardIssuer testIssuer;
+
+				//create a self checkout station and the components necessary to create it
+				Currency currency = Currency.getInstance("CAD");
+				int[] noteDenominations = {5,10,20,50,100};
+				BigDecimal[] coinDenomonations = {BigDecimal.valueOf(0.05) , BigDecimal.valueOf(0.10), BigDecimal.valueOf(0.25), 
+						BigDecimal.valueOf(1.0), BigDecimal.valueOf(2.0)};
+				int maxWeight = 23000;
+				int scaleSensitivity = 10;
+				testStation = new SelfCheckoutStation(currency, noteDenominations, coinDenomonations, maxWeight,scaleSensitivity);
+				
+				//create an issuer to manage payments
+				testIssuer = new CardIssuer("testIssuer");
+				Calendar testCalendar =  Calendar.getInstance();
+				testCalendar.set(Calendar.YEAR, 2030);
+				testIssuer.addCardData("111111", "Person One", testCalendar, "111", new BigDecimal("1000"));
+				//.............................................................................................
+				
+				PayWithDebit debit = new PayWithDebit(testStation);
+				Card testCard = new Card("debit", "111111", "Person One", "111", "1234", false, true);
+				debit.PayWithTap(testCard, testIssuer, BigDecimal.valueOf(100.0));
+	}
+	
 }
